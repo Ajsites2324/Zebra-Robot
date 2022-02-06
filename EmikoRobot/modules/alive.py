c0390b264a980044c34214b406c762a599248465
Ajsites2324 <97234991+Ajsites2324@users.noreply.github.com>
2022-02-06 13:49:40 +0530

    Update alive.py

--- EmikoRobot/modules/alive.py
+++ EmikoRobot/modules/alive.py
@@ -9,16 +9,16 @@ from EmikoRobot.events import register
 from EmikoRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/4f0e3eaffec2656e1986e.jpg"
+PHOTO = "https://te.legra.ph/file/99d03d5f83650aa3f4afe.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ 🆉ᴇʙʀᴀ 🆁ᴏʙᴏᴛ.** \n\n"
+  TEXT = f"**ʜɪ [{event.sender.first_name}](tg://user?id={event.sender.id}), ɪ'ᴍ 🆃🄿🅽 🆁ᴏʙᴏᴛ.** \n\n"
   TEXT += "⚪ **ɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ** \n\n"
-  TEXT += f"⚪ **ᴍʏ ᴍᴀsᴛᴇʀ : [sᴜᴍɪᴛ ʏᴀᴅᴀᴠ](https://t.me/Simple_Mundaa)** \n\n"
+  TEXT += f"⚪ **ᴍʏ ᴍᴀsᴛᴇʀ : [ᴀᴊᴇᴇᴛ ɢᴏɴᴅ](https://t.me/Papa_bol_sakteho)** \n\n"
   TEXT += f"⚪ **ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{telever}` \n\n"
   TEXT += f"⚪ **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{tlhver}` \n\n"
   TEXT += f"⚪ **ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{pyrover}` \n\n"
   TEXT += "**ᴛʜᴀɴᴋs ғᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Zebra_1Robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/World_FriendShip_Zone")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/Zebra_1Robot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/modern_elements")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
